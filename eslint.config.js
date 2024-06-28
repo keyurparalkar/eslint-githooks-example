478ccf22b9c6eefa7e58df7f63a20b39cfb606fd
keyurparalkar <keur.plkar@gmail.com>
2024-06-28 19:40:53 +0530

    chore: remove js recommended eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,6 @@
-import js from "@eslint/js";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 export default [
-	js.configs.recommended,
-
 	{
 		plugins: {
 			"simple-import-sort": simpleImportSort,
